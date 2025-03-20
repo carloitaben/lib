367ea7eca24521957b0531e2bbf6cde3adf3969d
Carlo <carlo@dallonses.com>
2025-03-20 15:39:51 +0100

    sync

--- src/lib/attempt.test.ts
+++ src/lib/attempt.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, test, vi } from "vitest"
-import { attempt, AttemptError, AttemptSuccess } from "./attempt"
+import type { AttemptError, AttemptSuccess } from "./attempt"
+import { attempt } from "./attempt"
 
 describe(attempt.name, () => {
   function mock(a: number, b: number) {
@@ -24,6 +25,7 @@ describe(attempt.name, () => {
     })
   }
 
+  test("sync error", () => {
     const result = attempt(() => mock(0, 0))
     expect(result.success).toBe(false)
     expect((result as AttemptError).error).toBeInstanceOf(Error)
@@ -36,23 +38,13 @@ describe(attempt.name, () => {
   })
 
   test("async error", async () => {
-    const result = await attempt(asyncMock, 0, 0)
+    const result = await attempt(() => asyncMock(0, 0))
     expect(result.success).toBe(false)
     expect((result as AttemptError).error).toBeInstanceOf(Error)
   })
 
   test("async success", async () => {
-    const result = await attempt(asyncMock, 1, 1)
-    expect(result.success).toBe(true)
-    expect((result as AttemptSuccess<number>).value).toBe(2)
-  })
-
-  test("bind", () => {
-    const binded = attempt.bind(null, (a: number, b: number) => a + b)
-    expect(binded).toBeInstanceOf(Function)
-
-    attempt.bind()
-    const result = binded(1, 1)
+    const result = await attempt(() => asyncMock(1, 1))
     expect(result.success).toBe(true)
     expect((result as AttemptSuccess<number>).value).toBe(2)
   })
--- src/lib/attempt.ts
+++ src/lib/attempt.ts
@@ -1,95 +1,68 @@
-// export type AttemptSuccess<Value> = {
-//   success: true
-//   value: Value
-// }
+export type AttemptSuccess<Value> = {
+  success: true
+  value: Value
+}
 
-// export type AttemptError = {
-//   success: false
-//   error: unknown
-// }
+export type AttemptError = {
+  success: false
+  error: unknown
+}
 
-// export type Attempt<Value> = AttemptSuccess<Value> | AttemptError
+export type Attempt<Value> = AttemptSuccess<Value> | AttemptError
 
-// export function isSuccessfulAttempt<Value>(
-//   attempt: Attempt<Value>
-// ): attempt is AttemptSuccess<Value> {
-//   return attempt.success === true
-// }
+export function isSuccessfulAttempt<Value>(
+  attempt: Attempt<Value>
+): attempt is AttemptSuccess<Value> {
+  return attempt.success === true
+}
 
-// export function isFailedAttempt<Value>(
-//   attempt: Attempt<Value>
-// ): attempt is AttemptError {
-//   return attempt.success === false
-// }
+export function isFailedAttempt<Value>(
+  attempt: Attempt<Value>
+): attempt is AttemptError {
+  return attempt.success === false
+}
 
-// function attemptPromise<Value>(value: Value | Promise<Value>) {
-//   if (value instanceof Promise) {
-//     return value
-//       .then((value) => attemptPromise(value))
-//       .catch((error) => ({
-//         success: false,
-//         error,
-//       }))
-//   }
+/**
+ * Attempts to execute a function with the provided arguments.
+ * If the function throws an error, it catches the error and returns it as a value,
+ * similar to the [Errors as Values pattern](https://jessewarden.com/2021/04/errors-as-values.html).
+ *
+ * @example
+ * ```ts
+ * const result = attempt(() => JSON.parse(data))
+ *
+ * if (result.success) {
+ *   result.data
+ * } else {
+ *   result.error
+ * }
+ * ```
+ */
+function resolve<Value>(value: Value): unknown {
+  if (value instanceof Promise) {
+    return value
+      .then((value) => resolve(value))
+      .catch((error) => ({
+        success: false,
+        error,
+      }))
+  }
 
-//   return {
-//     success: true,
-//     value,
-//   }
-// }
+  return {
+    success: true,
+    value,
+  }
+}
 
-// /**
-//  * Attempts to execute a function with the provided arguments.
-//  * If the function throws an error, it catches the error and returns it as a value,
-//  * similar to the [Errors as Values pattern](https://jessewarden.com/2021/04/errors-as-values.html).
-//  *
-//  * @example
-//  * ```ts
-//  * const result = attempt(() => JSON.parse(data))
-//  *
-//  * if (result.success) {
-//  *   result.data
-//  * } else {
-//  *   result.error
-//  * }
-//  * ```
-//  *
-//  * @example
-//  * ```ts
-//  * const safe = attempt.bind((a: number, b: number) => a + b, 1)
-//  * ```
-//  */
-// export function attempt<Args extends unknown[], Value>(
-//   fn: (...args: Args) => Value,
-//   ...args: Args
-// ): Value extends Promise<unknown>
-//   ? Promise<Attempt<Awaited<Value>>>
-//   : Attempt<Value> {
-//   try {
-//     return attemptPromise(fn(...args))
-//   } catch (error) {
-//     return {
-//       success: false,
-//       error,
-//     } as any
-//   }
-// }
-
-export function attempt<Args extends [unknown[]], Value>(
-  callback: (...args: [Args]) => Value,
-  ...args: [...Args]
-) {
+export function attempt<Result>(
+  callback: () => Result
+): Result extends Promise<infer V> ? Promise<Attempt<V>> : Attempt<Result> {
   try {
-    return callback(...args)
-  } catch (error) {}
+    return resolve(callback()) as any
+  } catch (error) {
+    return {
+      success: false,
+      error,
+    } as any
+  }
 }
-
-const foo = attempt(() => Math.random(), [])
-//    ^?
-
-function fn(a: number, b: number) {
-  return a + b
-}
-
-const bar = attempt.bind(null, fn)
-//    ^?
