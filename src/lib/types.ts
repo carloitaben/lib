6f3f02a8f7e3473a8980f4aecb4dc5d26bbb7110
Carlo <carlo@dallonses.com>
2025-03-24 10:19:59 +0100

    sync

---
+++ src/lib/types.ts
@@ -0,0 +1,26 @@
+/**
+ * A type guard that ensures a given array has at least one member.
+ *
+ * @example
+ * Basic usage
+ *
+ * ```ts
+ * const array = ["foo"]
+ *
+ * if (isNonEmptyArray(array)) {
+ *   array[0]
+ *   // ^? string
+ * }
+ * ```
+ */
+export function isNonEmptyArray<T>(array: T[]): array is [T, ...T[]] {
+  return Boolean(array.length)
+}
+
+export type Prettify<T> = {
+  [K in keyof T]: T[K]
+} & {}
+
+export type ValueOf<T> = T[keyof T]
+
+export type Suggest<Suggestion extends string> = Suggestion | (string & {})
